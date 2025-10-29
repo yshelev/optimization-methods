567df16b559273b4396e9317d54110a776faa4e5
zxcpisya <mamin.papa0@gmail.com>
2025-10-29 11:23:37 +1000

    123

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from bisection_method import bisection_method
-from func import f
-from golden_ratio_method import golden_ratio_method
+from gradient_descent_method import gradient_descent_method
+import numpy as np
+from func import A, B,
+x_k, f_xk = gradient_descent_method()
+
+print("вектор X: ")
+for i in x_k: 
+    print(f'{i:.4f}')
+
+print("Значение в (.) X: ")
+print(f_xk)
 
 
-interval = [-20, 9]
-epsilon = 0.005
-print(bisection_method(interval, epsilon))
-print(f)
-f.reset()
-print(golden_ratio_method(interval, epsilon))
-print(f)
+print("аналитическое решение (Ax = -b)")
+print(exec_sol := np.linalg.solve(A, -B))
+
+print(func)
