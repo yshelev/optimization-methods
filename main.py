f83d239b36fd0f3875d6af9df5bf0d7dc308bd9e
zxcpisya <mamin.papa0@gmail.com>
2025-10-29 11:12:31 +1000

    add exec solution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from gradient_descent_method import gradient_descent_method
 import numpy as np
-from func import A, B
+from func import A, B, func
 x_k, f_xk = gradient_descent_method()
 
 print("вектор X: ")
@@ -12,4 +12,6 @@ print(f_xk)
 
 
 print("аналитическое решение (Ax = -b)")
-print(np.linalg.solve(A, -B))
+print(exec_sol := np.linalg.solve(A, -B))
+
+print(func(exec_sol))
